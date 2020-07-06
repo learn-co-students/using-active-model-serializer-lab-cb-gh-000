f17a69222933ea7ac500f391316cc89f3c379f51
Sarmad Riaz <ms30237@gmail.com>
2020-07-06 22:02:33 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
+require_relative '../my_server.rb'
+require 'rack'
+require 'rack/test'
+
+def app() 
+  MyServer.new
+end
+
 RSpec.configure do |config|
 
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
+  config.include Rack::Test::Methods
+  config.order = 'default'
 end
